06daf6b714ffced70dcd4d6505350c3dda0d646f
Daniel <daniel.ellis.professional+git@gmail.comcd>
2022-12-12 02:00:46 +0000

    demo

--- src/script.js
+++ src/script.js
@@ -1,65 +1,72 @@
-var map = (window.map = new maplibregl.Map({
+import maplibregl from 'maplibre-gl';
+
+
+
+if ('SpeechSynthesisUtterance' in window) {
+  window.synth = window.speechSynthesis;
+  window.msg = new SpeechSynthesisUtterance();
+  msg.text = 'speech tools enabled - "context menu". Use two finger click on the trackpad to trigger'
+  synth.speak(msg);
+ }
+
+const map = new maplibregl.Map ({
   container: 'map',
-  zoom: 12,
-  center: [11.39085, 47.27574],
-  pitch: 52,
-  hash: true,
+  zoom: 7,
+  center: [-0.0692413790966953, 51.94653213902913],
   style: {
-  version: 8,
-  sources: {
-  osm: {
-  type: 'raster',
-  tiles: ['https://a.tile.openstreetmap.org/{z}/{x}/{y}.png'],
-  tileSize: 256,
-  attribution: '&copy; OpenStreetMap Contributors',
-  maxzoom: 19
+    version: 8,
+    sources: {
+      osm: {
+        type: 'raster',
+        tiles: ['https://a.tile.openstreetmap.org/{z}/{x}/{y}.png'],
+        tileSize: 256,
+        attribution: '&copy; OpenStreetMap Contributors',
+        maxzoom: 19,
+      },
+    },
+    layers: [
+      {
+        id: 'osm',
+        type: 'raster',
+        source: 'osm',
+      },
+    ],
   },
-  terrainSource: {
-  type: 'raster-dem',
-  url: 'https://demotiles.maplibre.org/terrain-tiles/tiles.json',
-  tileSize: 256
-  },
-  hillshadeSource: {
-  type: 'raster-dem',
-  url: 'https://demotiles.maplibre.org/terrain-tiles/tiles.json',
-  tileSize: 256
-  }
-  },
-  layers: [
-  {
-  id: 'osm',
-  type: 'raster',
-  source: 'osm'
-  },
-  {
-  id: 'hills',
-  type: 'hillshade',
-  source: 'hillshadeSource',
-  layout: { visibility: 'visible' },
-  paint: { 'hillshade-shadow-color': '#473B24' }
-  }
-  ],
-  terrain: {
-  source: 'terrainSource',
-  exaggeration: 1
-  }
-  },
-  maxZoom: 18,
-  maxPitch: 85
-  }));
-   
-  map.addControl(
-  new maplibregl.NavigationControl({
-  visualizePitch: true,
-  showZoom: true,
-  showCompass: true
-  })
-  );
-   
-  map.addControl(
-  new maplibregl.TerrainControl({
-  source: 'terrainSource',
-  exaggeration: 1
-  })
-  );
- 
+});
+
+map.on ('load', function () {
+  fetch ('/ons-district21-bgc.geojson')
+    .then (d => d.json ())
+    .then (geoJSONcontent => {
+      console.log (geoJSONcontent);
+      // Add as source to the map
+      map.addSource ('geojson-source', {
+        type: 'geojson',
+        data: geoJSONcontent,
+      });
+
+      map.addLayer ({
+        id: 'uploaded-polygons',
+        type: 'fill',
+        source: 'geojson-source',
+        paint: {
+          'fill-color': 'rgba(255,255,255,.5)',
+          'fill-outline-color': 'black',
+          'fill-opacity': 1,
+        },
+        filter: ['==', '$type', 'Polygon'],
+      });
+    })
+    .then (() => {
+      // create the event listener
+      map.on ('contextmenu', 'uploaded-polygons', function (e) {
+        const firstfeature = e.features[0];
+        const properties = firstfeature.properties;
+        var name = properties.areanm;
+
+        console.log (name, properties);
+        window.msg.text = `Area ${name}`
+        window.synth.speak (msg);
+      });
+    });
+});
